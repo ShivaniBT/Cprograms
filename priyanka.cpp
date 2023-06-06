fbe1dc4dcc3c356091a3cfedeca144d1b471bf24
[Shivani Bhardwaj] <[shivani061btit22@igdtuw.ac.in]>
2023-06-06 17:59:23 +0530

    priyanka.cpp added

---
+++ priyanka.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << "Priyanka pagal hai!!!\n";
+        cout << "Priyanka is stupid too.\n";
+    }
+    return 0;
+}
